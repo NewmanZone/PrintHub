f630e3814e33529c4dd6b8401669b7093ab94a6a
TARSCoder <3558889+tars-coder@users.noreply.github.com>
2026-05-11 00:49:50 -0700

    fix(build): add missing logging packages and using directive to Program.cs

--- src/PrintHub.Worker/Program.cs
+++ src/PrintHub.Worker/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
